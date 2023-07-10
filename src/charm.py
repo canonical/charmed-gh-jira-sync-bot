c66b7b9aa0cb57cee72981e6091b97372e703263
Maksim Beliaev <beliaev.m.s@gmail.com>
2023-07-10 12:18:43 +0200

    added config

--- src/charm.py
+++ src/charm.py
@@ -2,7 +2,6 @@ import logging
 
 import ops
 
-# Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
 
@@ -10,40 +9,44 @@ class GitHubJiraBotCharm(ops.CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.gh_jira_bot_pebble_ready, self._on_httpbin_pebble_ready)
+        self.framework.observe(self.on.gh_jira_bot_pebble_ready, self._on_config_changed)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
 
-    def _on_httpbin_pebble_ready(self, event: ops.PebbleReadyEvent):
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Add initial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", self._pebble_layer, combine=True)
-        # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
-        container.replan()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ops.ActiveStatus()
-
     def _on_config_changed(self, event: ops.ConfigChangedEvent):
-        log_level = self.model.config["log-level"].lower()
+        port = self.config["port"]
 
-        container = self.unit.get_container("httpbin")
-        # Verify that we can connect to the Pebble API in the workload container
+        container = self.unit.get_container("gh-jira-bot")
         if container.can_connect():
             # Push an updated layer with the new config
-            container.add_layer("httpbin", self._pebble_layer, combine=True)
+            container.add_layer("gh_jira_bot", self._pebble_layer, combine=True)
             container.replan()
 
-            logger.debug("Log level for gunicorn changed to '%s'", log_level)
             self.unit.status = ops.ActiveStatus()
         else:
             # We were unable to connect to the Pebble API, so we defer this event
             event.defer()
-            self.unit.status = ops.WaitingStatus("waiting for Pebble API")
+            self.unit.status = ops.WaitingStatus("Waiting for Pebble API")
 
+    @property
+    def app_environment(self):
+        env = {
+            "APP_ID": self.config["app-id"],
+            "PRIVATE_KEY": self.config["private-key"],
+            "WEBHOOK_SECRET": self.config["webhook-secret"],
+            "JIRA_INSTANCE": self.config["jira-instance"],
+            "JIRA_USERNAME": self.config["jira-username"],
+            "JIRA_TOKEN": self.config["jira-token"]
+        }
+        if bot_config := self.config["bot-config"]:
+            env["DEFAULT_BOT_CONFIG"] = bot_config
+        if bot_name := self.config["bot-name"]:
+            env["BOT_NAME"] = bot_name
+
+        return env
 
     @property
     def _pebble_layer(self):
+
         return {
             "summary": "httpbin layer",
             "description": "pebble config layer for httpbin",
@@ -53,9 +56,7 @@ class GitHubJiraBotCharm(ops.CharmBase):
                     "summary": "httpbin",
                     "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
                     "startup": "enabled",
-                    "environment": {
-                        "GUNICORN_CMD_ARGS": f"--log-level {self.model.config['log-level']}"
-                    },
+                    "environment": self.app_environment,
                 }
             },
         }
